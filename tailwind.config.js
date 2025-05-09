c74d9d603dcf8728249b91381155a43dc8360982
njay <ninogaringarao13@gmail.com>
2025-05-09 20:39:13 +0800

    Modify (color.js, tailwindconfig.js) : update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DDA853",
-        background: "#FBF5DD",
-        secondary: "#16404D",
+        primary: "#47A897",
+        background: "#CFFCF9",
+        secondary: "#9DCAC5",
         panel: "#A6CDC6",
         uBlack: "#1f2937",
         uGray: "#4b5563",
         uGrayLight: "#9ca3af",
         failed: "#dc2626",
-        pending: "#9ca3af",
-        processing: "#3b82f6",
-        pickup: "#f97316",
         success: "#22c55e",
       },
     },
